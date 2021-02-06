b2a8c83cdf83afd3e51c3b394b69e457a5bdc8fe
berrybab6 <bedatuassefa@gmail.com>
2021-02-06 16:58:37 +0300

    user registration done

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import psycopg2.extras
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
+
+#from app import  userInfo
+#from app import db
+from flask import request, render_template,jsonify
+from datetime import datetime as dt
+import hashlib
+#from app import userInfo
+from flask import current_app as app
+
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity
@@ -14,10 +23,11 @@ ENV = 'pro'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    #app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:berrybab0764@localhost:5432/TikusEvent'
 else:
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xuijhtqmoyikzo:15b8cb464e2fad66693e89dcb4d8e8b8b93b789281259de9a822399a536a42a4@ec2-54-159-113-254.compute-1.amazonaws.com:5432/dt563jahevl50'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:berrybab0764@localhost:5432/TikusEvent'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -44,6 +54,7 @@ class userInfo(db.Model):
 app.config['JWT_SECRET_KEY'] = 'qwertyuioplmnbvcxzasdfghjk'
 jwt = JWTManager(app)
 
+#from routes import register
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return jsonify({'message': 'Hello World!', 'User': 'Test User', 'requesData': request.args.get('abc')})
@@ -88,7 +99,56 @@ def hello():
     else:
         return 'Hello'
 
+@app.route("/register", methods=["POST","GET"])
+def register():
+    user = request.get_json()
+    #print (content)
+    username1=user["user_name"]
+    email1=user["email"]
+    #user_id=user["user_id"]
+    password1=user["password"]
+    created_on1=dt.now()
+    profile_url1="t.me/berrybab6"
+    #followers=0
+    
+    if email1 == "":
+        message = "Please fill in your email address"
+        return jsonify({"message":message})
+        
+
+    if password1 == "":
+        message = "Please fill in your password"
+        return jsonify({"message":message})
+    if email1 and password1:
+        is_exist=userInfo.query.filter(
+            userInfo.user_name == username1 or userInfo.email == email1
+        ).first()
+        if is_exist:
+            return jsonify({"message":"User already Exists"})
+
+    #Hash Password
+    hashed = hashlib.sha256(password1.encode())
+    password = hashed.hexdigest()
+    #db_conn = psycopg2.connect(host=db_host, port=db_port, dbname=dbname, user=db_user, password=pw)
+    #db_cursor = db_conn.cursor()
+
+    new_user = userInfo(user_name=username1,password=password,email=email1,created_on=created_on1,profile_url=profile_url1,followers=10)
+    db.session.add(new_user)
+    #db_conn.session.commit()
+
+
+    #db_cursor.execute(new_user)
+    try:
+        db.session.commit()
+    except psycopg2.Error as e:
+        message = "Database error: " + e + "/n SQL: " + new_user
+        return jsonify({"message":message}),401 #render_template("register.html", message = t_message)
+
+    message = "Your user account has been added."
+    return jsonify({"message":message,"username":username1})
+
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
+#from routes import register
